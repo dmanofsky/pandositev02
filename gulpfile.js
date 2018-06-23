63ed19017dd9dd85c9e769b5db66066849713568
Douglas Manofsky <douglasmanofsky@Douglass-MacBook-Pro.local>
2018-06-23 12:40:15 -0400

    production gulp tasks 1

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,38 @@ gulp.task('message', () =>
     console.log('Gulp is running...')
 );
 
-// Optimize images
+// -------------------------------prototype tasks ----------------------------------------
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', () =>
+    gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream())
+);
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./src",
+        port: 3000
+    })
+
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass'])
+    gulp.watch("src/*.html").on('change', browserSync.reload)
+});
+
+// -------------------------------production tasks ----------------------------------------
+
+// Move the HTML files into the /dist folder
+gulp.task('html', () =>
+    gulp.src('src/*.html')
+        .pipe(gulp.dest("dist/"))
+        .pipe(browserSync.stream())
+);
+
+// Optimize images for dist/assets
 gulp.task('imageMin', function() {
 	gulp.src('src/assets/icons/*')
 		.pipe(imagemin())
@@ -22,38 +53,22 @@ gulp.task('imageMin', function() {
 		.pipe(gulp.dest('dist/assets/logos'));
 });
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', () =>
-    gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
-        .pipe(sass())
+// Move the CSS files into the /dist/css folder
+gulp.task('css', () =>
+    gulp.src('src/css/*')
         .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream())
 );
 
-// Move the javascript files into our /dist/js folder
+// Move the javascript files into the /dist/js folder
 gulp.task('js', () =>
     gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
         .pipe(gulp.dest("dist/js"))
         .pipe(browserSync.stream())
 );
 
-// Move the HTML files into our /dist folder
-gulp.task('html', () =>
-    gulp.src('src/*.html')
-        .pipe(gulp.dest("dist/"))
-        .pipe(browserSync.stream())
-);
+//-----------------------------------high level tasks------------------------------------
 
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('default', ['message','serve']);
 
-    browserSync.init({
-        server: "./dist",
-        port: 80
-    })
-
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass'])
-    gulp.watch("src/*.html").on('change', browserSync.reload)
-});
-
-gulp.task('default', ['html','js','serve']);
+gulp.task('production', ['message','html','imageMin','sass','css','js'])
